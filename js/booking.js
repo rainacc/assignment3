5e7ebad6c087fff5e291987b39f77311f95fc5b8
rainacc <ycchi1@student.unimelb.edu.au>
2023-05-19 23:41:56 +1000

    Home/Form change/svg added

--- js/booking.js
+++ js/booking.js
@@ -1,4 +1,19 @@
 //Check reservation date, avoid selecting passed date
 let today = new Date().toISOString().split("T")[0];
-let dateInput = document.getElementById("date");
+let dateInput = document.getElementById("date-id");
 dateInput.setAttribute("min", today);
+
+//Change Book form steps//
+function nextBookStep(n) {
+let currentForm = "step" + n;
+let nextForm = "step" + (n + 1);
+document.getElementById(currentForm).style.display = "none";
+document.getElementById(nextForm).style.display = "block";
+}
+
+function goBackBookStep(n){
+    let currentForm = "step" + n;
+    let prevForm = "step" + (n - 1);
+    document.getElementById(currentForm).style.display = "none";
+    document.getElementById(prevForm).style.display = "block";
+}
--- js/interaction.js
+++ js/interaction.js
@@ -1,68 +1,39 @@
-//hover interaction for navigation button in mobile version
-function changeNavButton() {
-  document.getElementById("navButton").src = "../images/icons/navButton_hover.png";
-}
 
-function resetNavButton() {
-  document.getElementById("navButton").src = "../images/icons/navButton.png";
-}
-  
 //Slideshow for mobile home page
-let slideIndexHome = -1;
-function showSlidesHome() {
-  let slidesH = document.getElementsByClassName("slideImages-home");
-  for (let i = 0; i < slidesH.length; i++) {
-    slidesH[i].classList.remove("show");
+let slideIndex = -1;
+function showSlides() {
+  let slides = document.getElementsByClassName("slideImages");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("show");
   }
   
-  slideIndexHome++;
+  slideIndex++;
   
-  if (slideIndexHome >= slidesH.length) {
-    slideIndexHome = 0;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
   }  
   
-  slidesH[slideIndexHome].classList.add("show");
+  slides[slideIndex].classList.add("show");
 }
 
 if (window.location.pathname.endsWith("index.html")) {
   window.onload = function() {
-    showSlidesHome(); 
-    setInterval(showSlidesHome, 3000);
+    showSlides(); 
+    setInterval(showSlides, 3000);
   };
 }
-  
-
-//Slideshow for mobile about us page
-let slideIndexAbout = -1;
-function showSlidesAbout() {
-  let slidesA = document.getElementsByClassName("slideImages-about");
-  for (let i = 0; i < slidesA.length; i++) {
-    slidesA[i].classList.remove("show");
-  }
-  
-  slideIndexAbout++;
-  
-  if (slideIndexAbout >= slidesA.length) {
-    slideIndexAbout = 0;
-  }  
-  
-  slidesA[slideIndexAbout].classList.add("show");
-}
-
-if (window.location.pathname.endsWith("about.html")) {
-  window.onload = function() { //run slideshow upon page open
-    showSlidesAbout(); 
-    setInterval(showSlidesAbout, 3000);
-  };
-}
-
 
 const navigationPage = document.getElementById("navPage");
 const closeNavButton = document.getElementById("closeNavPageButton");
 const openNavButton = document.getElementById("navButton");
 
 function openNav() {
-  navPage.style.display = "block";
+  var x = document.getElementById("navPage");
+  if (x.className === "navButtons") {
+    x.className += " responsive";
+  } else {
+    x.className = "navButtons";
+  }
 }
 
 function closeNav(){
