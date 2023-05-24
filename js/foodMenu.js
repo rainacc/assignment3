69b6cc90b8a1ba66db5bcd9830f8d56b9cce58b1
rainacc <ycchi1@student.unimelb.edu.au>
2023-05-24 11:36:09 +1000

    food card finished

--- js/foodMenu.js
+++ js/foodMenu.js
@@ -1,7 +1,9 @@
 function showFood(foodId){
-    document.getElementById(foodId).style.display ="block";
+    document.getElementById(foodId).style.display ="block"; //Show food card details with dark overlay on background
+    document.getElementsByClassName('scrollDownField')[0].style.display = "none";//Hide next page button when open up food detail
 }
 
 function hideFood(foodId){
     document.getElementById(foodId).style.display ="none";
+    document.getElementsByClassName('scrollDownField')[0].style.display = "block";
  }
